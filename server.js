782434820e2a8aa51dc6d254f170d7efebd7c02a
Thiago Mariotto <thiagomariotto@outlook.com.br>
2021-07-28 19:42:19 -0300

    deploy

---
+++ server.js
@@ -0,0 +1,10 @@
+const express = require('express')
+const app = express()
+const port = 3000
+
+
+app.set('view engine', 'ejs');
+app.use('/public', express.static(__dirname + '/public'));
+
+app.get('/', (req, res) => res.status(200).render('index'));
+app.listen(port, () => console.log(`Example app listening on port port!`))
